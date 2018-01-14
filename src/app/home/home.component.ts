10cc44ebe797054c315b0010aa09c22639be66a7
Aaron Goodrum <ag505181@gmail.com>
2018-01-13 18:08:49 -0800

    end routes

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-home',
@@ -37,11 +38,13 @@ export class HomeComponent implements OnInit {
   goalText: string = 'My first life goal';
   goals = [];
 
-  constructor() { }
+  constructor(private _data: DataService) { }
 
   ngOnInit() {
     // itemCount to the length (or number) of goals in the goal array
     this.itemCount = this.goals.length;
+    this._data.goal.subscribe(res => this.goals = res);
+    this._data.changeGoal(this.goals);
   }
 
   addItem() {
@@ -49,9 +52,11 @@ export class HomeComponent implements OnInit {
     this.goals.push(this.goalText);
     this.goalText = '';
     this.itemCount = this.goals.length;
+    this._data.changeGoal(this.goals);
   }
 
-  removeItem(i){
+  removeItem(i) {
     this.goals.splice(i, 1);
+    this._data.changeGoal(this.goals);
   }
 }
